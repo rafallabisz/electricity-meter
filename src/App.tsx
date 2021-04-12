aa903ad96afbb72faa4d1f266d0394df2041271c
Rafal Labisz <rafal0710@gmail.com>
2021-04-12 20:39:19 +0200

    add img electricity meter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ElectricityMeterIcon from './assets/electricity-meter.jpg';
 
 interface AppProps {
   
@@ -6,9 +7,12 @@ interface AppProps {
 
 const App: React.FC<AppProps> = (props) => {
   return (
-    <div>
-      Licznik
-    </div>
+    <main>
+      <h2>Licznik prądu</h2>
+      <div>
+        <img src={ElectricityMeterIcon} alt="licznik prądu" width={300} />
+      </div>
+    </main>
   )
 }
 export default App
